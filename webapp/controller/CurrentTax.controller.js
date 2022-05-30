82626311158a092a76b912f04ba673001cbdb49b
Camilla Sarra <csarra@deloitte.it>
2022-05-30 14:13:02 +0000

    aggiunta pagina per riprese

--- webapp/controller/CurrentTax.controller.js
+++ webapp/controller/CurrentTax.controller.js
@@ -2,12 +2,16 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     'sap/ui/model/Filter',
     'sap/ui/model/FilterOperator',
-    'sap/ui/model/json/JSONModel'
+    'sap/ui/model/json/JSONModel',
+    "sap/ui/table/RowAction",
+	"sap/ui/table/RowActionItem",
+	"sap/ui/table/RowSettings",
+	"sap/ui/core/routing/History"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, Filter, FilterOperator, JSONModel) {
+    function (Controller, Filter, FilterOperator, JSONModel, RowAction, RowActionItem, RowSettings, History) {
         "use strict";
 
         return Controller.extend("tax.provisioning.computations.controller.CurrentTax", {
@@ -21,9 +25,27 @@ sap.ui.define([
                 this._setHeader();
             },
 
+            handleActionPress : function(oEvent) {
+                debugger;
+                var oRouter = this.getOwnerComponent().getRouter();
+                debugger;
+                    oRouter.navTo("RouteRiprese", {
+                        ripresaID : oEvent.getSource().getBindingContext("oModelAnagrafica").getObject().ID,
+                        ID : this.getView().getModel("computationModel").getData().oModel[0].ID
+                        //configurazioneID :  this.getView().getModel("computationModel").getData().oModel[0].configurazioneID
+                    }, true);
+            },
+
             _onObjectMatched: function (oEvent) {
                 var oEvent = oEvent.getParameter("arguments");
-                var ID = oEvent.ID;
+                
+                var ID = oEvent.ID; //ID computazione
+                
+
+                this.getView().setModel(new JSONModel({oModel: [{
+                    "ID": ID
+                  }]}), "computationModel");
+                
                 var that = this;
                 //lettura modello AnagraficaRiprese
                 jQuery.ajax({
@@ -72,14 +94,6 @@ sap.ui.define([
                     "impostaPerc": 24,
                     "testo": "Utile perdita ante imposte"
                   }]}), "headerModel");
-            },
-
-            sumHeaders: function(fValue){
-                debugger;
-                if (fValue) {
-                    return "> " + Math.floor(fValue/1000000) + "M";
-                }
-                return "0";
             }
         });
     });
---
+++ webapp/controller/Riprese.controller.js
@@ -0,0 +1,110 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
+    'sap/ui/model/json/JSONModel',
+    "sap/ui/table/RowAction",
+	"sap/ui/table/RowActionItem",
+	"sap/ui/table/RowSettings",
+	"sap/ui/core/routing/History"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, Filter, FilterOperator, JSONModel, RowAction, RowActionItem, RowSettings, History) {
+        "use strict";
+
+        return Controller.extend("tax.provisioning.computations.controller.Riprese", {
+            onInit: function () {
+                this._bDescendingSort = false;
+                this.oProductsTable = this.oView.byId("productsTable");
+                this.oModel = this.getOwnerComponent().getModel();
+                this.getOwnerComponent().getRouter().getRoute("RouteRiprese").attachPatternMatched(this._onObjectMatched, this);
+                
+                sap.ui.getCore().sapAppID = this.getOwnerComponent().getMetadata().getManifest()["sap.app"].id;
+                //this.getView().getModel("oModelAnagrafica");
+
+                this.oView = this.getView();
+            
+                sap.ui.getCore().sapAppID = this.getOwnerComponent().getMetadata().getManifest()["sap.app"].id;
+
+                this.getView().getModel("oModelAnagraficaSingola");
+            },
+
+            _onObjectMatched: function (oEvent) {
+                var oEvent = oEvent.getParameter("arguments");
+                
+                var ID = oEvent.ID; //ID computazione
+                var ripresaID = oEvent.ripresaID; //ID ripresa singola
+
+                var that = this;
+
+                jQuery.ajax({
+                    url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/ComputationsView("+ID+")"),
+                    contentType: "application/json",
+                    type: 'GET',
+                    dataType: "json",
+                    async: false,
+                    success: function (oCompleteEntry) {
+                        var data = {
+                            oModelconfigurazione: oCompleteEntry.d.configurazioneID
+                        };
+                        var DataModel = new sap.ui.model.json.JSONModel();
+                        DataModel.setData(data);
+                        that.getView().setModel(DataModel, "oModelConfigurazione");
+                        that._setRipresa(oCompleteEntry.d.configurazioneID, ripresaID, ID);
+                    },
+                    error: function (error) {
+                        sap.m.MessageToast.show("Error");
+                    }
+                });
+
+                //var configurazioneID = this.getView().getModel("oModelConfigurazione").getData().oModelconfigurazione; //ID configurazione
+                
+
+                                
+            },
+
+            _setRipresa: function(configurazioneID, ripresaID, ID){
+                var that = this;
+
+                jQuery.ajax({
+                    url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/Configurazioni/"+configurazioneID+"?$expand=versione($expand=items($expand=algoritmi($filter=codiceRipresa_ID eq '"+ripresaID+"' and configurazione_ID eq "+configurazioneID+";$expand=ripreseManuali($filter=computations_ID eq "+ID+"))))"),
+                    contentType: "application/json",
+                    type: 'GET',
+                    dataType: "json",
+                    async: false,
+                    success: function (oCompleteEntry) {
+                        var data = {
+                            oModel1: oCompleteEntry.d
+                        };
+                        var DataModel = new sap.ui.model.json.JSONModel();
+                        DataModel.setData(data);
+                        that.getView().setModel(DataModel, "oModelAnagraficaSingola");
+                    },
+                    error: function (error) {
+                        sap.m.MessageToast.show("Error");
+                    }
+                }); 
+
+                jQuery.ajax({
+                    url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/AnagraficaRiprese/"+ripresaID+"?$expand=configurazioni($filter=configurazione_ID eq "+configurazioneID+";$expand=configurazione($expand=versione($expand=items($expand=algoritmi($filter=codiceRipresa_ID eq '"+ripresaID+"' and configurazione_ID eq "+configurazioneID+";$expand=ripreseManuali($filter=computations_ID eq "+ID+"))))))"),
+                    contentType: "application/json",
+                    type: 'GET',
+                    dataType: "json",
+                    async: false,
+                    success: function (oCompleteEntry) {
+                        var data = {
+                            oModel2: oCompleteEntry.d
+                        };
+                        var DataModel = new sap.ui.model.json.JSONModel();
+                        DataModel.setData(data);
+                        that.getView().setModel(DataModel, "oModelAnagraficaSingolaTestata");
+                    },
+                    error: function (error) {
+                        sap.m.MessageToast.show("Error");
+                    }
+                }); 
+            }
+        });
+    });
