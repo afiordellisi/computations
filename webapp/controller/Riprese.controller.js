629e8cc44ed1de45042efd999524b33b72a70aaa
Camilla Sarra <csarra@deloitte.it>
2022-06-16 16:05:04 +0000

    fix riprese

--- webapp/controller/Riprese.controller.js
+++ webapp/controller/Riprese.controller.js
@@ -15,20 +15,25 @@ sap.ui.define([
     function (Controller, Filter, FilterOperator, JSONModel, RowAction, RowActionItem, RowSettings, History, BaseController) {
         "use strict";
         var configurazioneID;
+        var descrizioneComputazione;
+        //var ripresaID;
+        var versioneID;
+        var computazioneID;
+        //var imposta;
 
         return BaseController.extend("tax.provisioning.computations.controller.Riprese", {
             onInit: function () {
                 // this._bDescendingSort = false;
                 // this.oProductsTable = this.oView.byId("productsTable");
-                
+
                 this.oModel = this.getOwnerComponent().getModel();
                 this.getOwnerComponent().getRouter().getRoute("Riprese").attachPatternMatched(this._onObjectMatched, this);
-                
+
                 sap.ui.getCore().sapAppID = this.getOwnerComponent().getMetadata().getManifest()["sap.app"].id;
                 //this.getView().getModel("oModelAnagrafica");
 
                 this.oView = this.getView();
-            
+
                 sap.ui.getCore().sapAppID = this.getOwnerComponent().getMetadata().getManifest()["sap.app"].id;
 
                 this.getView().getModel("oModelAnagraficaSingola");
@@ -36,16 +41,18 @@ sap.ui.define([
 
             _onObjectMatched: function (oEvent) {
                 var oEvent = oEvent.getParameter("arguments");
-                
-                var ID = oEvent.ID; //ID computazione
+                debugger;
+                computazioneID = oEvent.ID; //ID computazione
                 var ripresaID = oEvent.ripresaID; //ID ripresa singola
-                configurazioneID = oEvent.configurazioneID;
+                var imposta = oEvent.imposta;
+                
+                this._setRipresa(ripresaID, computazioneID, imposta);
 
-                this._setRipresa(configurazioneID, ripresaID, ID);
-
-                this.getView().setModel(new JSONModel({oModel: [{
-                    "ID": ID
-                  }]}), "IDcomputationModel");
+                this.getView().setModel(new JSONModel({
+                    "ID": computazioneID,
+                    "ripresaID": ripresaID,
+                    "imposta" : imposta
+                  }), "IDcomputationModel");
 
                 // var that = this;
 
@@ -70,75 +77,143 @@ sap.ui.define([
                 // });
 
                 //var configurazioneID = this.getView().getModel("oModelConfigurazione").getData().oModelconfigurazione; //ID configurazione
-                
 
-                                
+
+
             },
 
             handleItemPress: function(oEvent){
-                
+
                 oEvent.getSource().getBindingContext("oModelAnagraficaSingola").getObject();
 
                 var oRouter = this.getOwnerComponent().getRouter();
                 oRouter.navTo("Allegati", {
-                    ID : this.getView().getModel("IDcomputationModel").getData().oModel[0].ID,
-                    ripresaID : this.getView().getModel("oModelAnagraficaSingolaTestata").getData().oModel2.ID,
-                    codiceGL : oEvent.getSource().getBindingContext("oModelAnagraficaSingola").getObject().codiceGL,
-                    descrizioneGL : oEvent.getSource().getBindingContext("oModelAnagraficaSingola").getObject().descrizioneGL,
-                    versioneID : oEvent.getSource().getBindingContext("oModelAnagraficaSingola").getObject().versione_ID,
-                    configurazioneID : configurazioneID
-                }, true);
+                    ID : this.getView().getModel("IDcomputationModel").getData().ID, //computazione ID
+                    ripresaID : this.getView().getModel("IDcomputationModel").getData().ripresaID,
+                    codiceGL : oEvent.getSource().getBindingContext("oModelAnagraficaSingola").getObject().bilancio_codiceGL,
+                    imposta: this.getView().getModel("IDcomputationModel").getData().imposta,
+                }, false);
             },
 
-            _setRipresa: function(configurazioneID, ripresaID, ID){
+            _setRipresa: function(ripresaID, computazioneID, imposta){
                 var that = this;
-
+                debugger;
                 jQuery.ajax({
-                    url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/Configurazioni/"+configurazioneID+"?$expand=versione($expand=trialBalances($expand=confRipresaAuto($filter=confRipresa_codiceRipresa_ID eq '"+ripresaID+"' and confRipresa_configurazione_ID eq "+configurazioneID+")))"),
+                    url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/ConfRipresaAutoView?$count=true&$expand=confRipresa($expand=codiceRipresa)&$filter=ID eq "+computazioneID+" and confRipresa_codiceRipresa_ID eq '"+ripresaID+"' and confRipresa_imposta eq '"+imposta+"'"),
                     contentType: "application/json",
                     type: 'GET',
                     dataType: "json",
                     async: false,
                     success: function (oCompleteEntry) {
-                        var percentuali = [];
-                        for(var i=0; i < oCompleteEntry.versione.trialBalances.length; i++){
-                            if(oCompleteEntry.versione.trialBalances[i].confRipresaAuto[0] !== undefined){
-                                percentuali.push(oCompleteEntry.versione.trialBalances[i].confRipresaAuto[0].percentuale)
-                            }else{
-                                percentuali.push("not configured")
-                            }
-                        }
+                        //var percentuali = [];
+                        // for(var i=0; i < oCompleteEntry.versione.trialBalances.length; i++){
+                        //     if(oCompleteEntry.versione.trialBalances[i].confRipresaAuto[0] !== undefined){
+                        //         percentuali.push(oCompleteEntry.versione.trialBalances[i].confRipresaAuto[0].percentuale)
+                        //     }else{
+                        //         percentuali.push("not configured")
+                        //     }
+                        // }
                         var data = {
-                            oModel1: oCompleteEntry.versione.trialBalances
+                            oModel1: oCompleteEntry.value,
+                            oModelRipresa: oCompleteEntry.value[0].confRipresa.codiceRipresa,
+                            oModelTestata: oCompleteEntry.value[0].confRipresa
                         };
                         var DataModel = new sap.ui.model.json.JSONModel();
                         DataModel.setData(data);
                         that.getView().setModel(DataModel, "oModelAnagraficaSingola");
+                        that._setTotali(computazioneID, imposta, ripresaID);
+                        //that.getView().getModel("oModelAnagraficaSingola").oData.imposta = imposta
                         //that.getView().getModel("oModelAnagraficaSingola").getData().oModel1.push(percentuali)
                     },
                     error: function (error) {
                         sap.m.MessageToast.show("Error");
                     }
-                }); 
+                });
 
+                // jQuery.ajax({
+                //     url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/Configurazioni/"+configurazioneID+"?$expand=versione($expand=trialBalances($expand=confRipresaAuto($filter=confRipresa_codiceRipresa_ID eq '"+ripresaID+"' and confRipresa_configurazione_ID eq "+configurazioneID+")))"),
+                //     contentType: "application/json",
+                //     type: 'GET',
+                //     dataType: "json",
+                //     async: false,
+                //     success: function (oCompleteEntry) {
+                //         var percentuali = [];
+                //         for(var i=0; i < oCompleteEntry.versione.trialBalances.length; i++){
+                //             if(oCompleteEntry.versione.trialBalances[i].confRipresaAuto[0] !== undefined){
+                //                 percentuali.push(oCompleteEntry.versione.trialBalances[i].confRipresaAuto[0].percentuale)
+                //             }else{
+                //                 percentuali.push("not configured")
+                //             }
+                //         }
+                //         var data = {
+                //             oModel1: oCompleteEntry.versione.trialBalances,
+                //             oModelVersione: oCompleteEntry.versione.ID
+                //         };
+                //         var DataModel = new sap.ui.model.json.JSONModel();
+                //         DataModel.setData(data);
+                //         that.getView().setModel(DataModel, "oModelAnagraficaSingola");
+                //         //that.getView().getModel("oModelAnagraficaSingola").oData.imposta = imposta
+                //         //that.getView().getModel("oModelAnagraficaSingola").getData().oModel1.push(percentuali)
+                //     },
+                //     error: function (error) {
+                //         sap.m.MessageToast.show("Error");
+                //     }
+                // });
+
+                // jQuery.ajax({
+                //     url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/AnagraficaRiprese/"+ripresaID+"?$expand=configurazioni($filter=configurazione_ID eq "+configurazioneID+")"),
+                //     contentType: "application/json",
+                //     type: 'GET',
+                //     dataType: "json",
+                //     async: false,
+                //     success: function (oCompleteEntry) {
+                //         var data = {
+                //             oModel2: oCompleteEntry
+                //         };
+                //         var DataModel = new sap.ui.model.json.JSONModel();
+                //         DataModel.setData(data);
+                //         that.getView().setModel(DataModel, "oModelAnagraficaSingolaTestata");
+                //     },
+                //     error: function (error) {
+                //         sap.m.MessageToast.show("Error");
+                //     }
+                // });
+
+                
+
+            },
+
+            _setTotali: function(computazioneID, imposta, ripresaID){
+                var that = this;
+                debugger;
                 jQuery.ajax({
-                    url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/AnagraficaRiprese/"+ripresaID+"?$expand=configurazioni($filter=configurazione_ID eq "+configurazioneID+")"),
+                    url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/ConfRipresaAutoView?$apply=filter(ID eq "+computazioneID+" and confRipresa_codiceRipresa_ID eq '"+ripresaID+"' and confRipresa_imposta eq '"+imposta+"')/aggregate(importoCY with sum as TotaleCY,ripresaAuto with sum as TotaleAuto,ripresaManuale with sum as TotaleManuale,totaleRipresa with sum as TotaleRipresa)"),
                     contentType: "application/json",
                     type: 'GET',
                     dataType: "json",
                     async: false,
                     success: function (oCompleteEntry) {
                         var data = {
-                            oModel2: oCompleteEntry
+                            oModelTot: oCompleteEntry.value
                         };
                         var DataModel = new sap.ui.model.json.JSONModel();
                         DataModel.setData(data);
-                        that.getView().setModel(DataModel, "oModelAnagraficaSingolaTestata");
+                        that.getView().setModel(DataModel, "oModelTotali");
+                        //that.getView().getModel("oModelAnagraficaSingola").oData.imposta = imposta
+                        //that.getView().getModel("oModelAnagraficaSingola").getData().oModel1.push(percentuali)
                     },
                     error: function (error) {
                         sap.m.MessageToast.show("Error");
                     }
-                }); 
+                });
+
+            },
+
+            onNavBack: function(oEvent){
+                this.getRouter().navTo("CurrentTax", {
+                    ID : this.getView().getModel("IDcomputationModel").getData().ID
+                    //ID : computazioneID
+                });
             }
         });
     });
