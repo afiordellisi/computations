828a7fc9f44bb31ba44ff4c22474f233d4e9d521
Camilla Sarra <csarra@deloitte.it>
2022-06-23 14:11:48 +0000

    smart table DT view

--- webapp/controller/DTLDTA.controller.js
+++ webapp/controller/DTLDTA.controller.js
@@ -2,20 +2,89 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     'sap/ui/model/json/JSONModel',
     'sap/m/MessageToast',
-    './BaseController'
+    './BaseController',
+	'sap/ui/model/odata/v2/ODataModel',
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel, MessageToast, BaseController) {
+    function (Controller, JSONModel, MessageToast, BaseController, ODataModel) {
         "use strict";
 
-        return BaseController.extend("tax.provisioning.computations.controller.TimeDiff", {
+        return BaseController.extend("tax.provisioning.computations.controller.DTLDTA", {
 
             onInit: function () {
 
                 sap.ui.getCore().sapAppID = this.getOwnerComponent().getMetadata().getManifest()["sap.app"].id;
-                this.getOwnerComponent().getRouter().getRoute("TimeDiff").attachPatternMatched(this._onObjectMatched, this);
+                this.getOwnerComponent().getRouter().getRoute("DTLDTA").attachPatternMatched(this._onObjectMatched, this);
+
+                var oModel, oView, sServiceUrl;
+                //this.getView().byId("LineItemsSmartTable").setModel("computationsModel");
+                sServiceUrl = jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/v2/catalog/");
+                oModel = new ODataModel(sServiceUrl, {
+                   defaultCountMode: "None"
+               });
+
+               oView = this.getView();
+               oView.setModel(oModel); 
+
+            },
+
+            setTable: function(oEvent){
+                console.log(oEvent)
+                var computationID=this.getView().getModel("oModelTestata").getData().computationID;
+                var imposta=this.getView().getModel("oModelTestata").getData().imposta;
+                var that = this;
+
+                var oSmartTable = oEvent.getSource();
+
+                //var oSmartFilterBar = this.byId(oSmartTable.getSmartFilterId());
+
+                //var path = oSmartFilterBar.getParameterBindingPath();
+                var path= "/DTView(imposta='"+imposta+"',computationId="+computationID+")/Set"
+
+                oSmartTable.setTableBindingPath(path);
+
+                // jQuery.ajax({
+                //     // url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/DTView(imposta='"+imposta+"',computationId="+computationID+")/Set"),
+                //     url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/Configurazioni"),
+                //     contentType: "application/json",
+                //     type: 'GET',
+                //     dataType: "json",
+                //     async: false,
+                //     success: function (oCompleteEntry) {
+                //         var data = oCompleteEntry.value;
+                //          var DataModel = new sap.ui.model.json.JSONModel();
+                //          DataModel.setData(data);
+                //          that.getView().setModel(DataModel, "");                         
+                //         //that.getView().getModel().setData(data);
+                //        // that.getView().getModel().oData = data
+                //     },
+                //     error: function (error) {
+                //         sap.m.MessageToast.show("Error");
+                //     }
+                // });
+
+                jQuery.ajax({
+                    url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/DTView(imposta='"+imposta+"',computationId="+computationID+")/Set"),
+                    // url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/Configurazioni"),
+                    // contentType: "application/json",
+                    type: 'GET',
+                    dataType: "json",
+                    async: false,
+                    success: function (oCompleteEntry) {
+                        var data = oCompleteEntry.value;
+                        var DataModel = new sap.ui.model.json.JSONModel();
+                        DataModel.setData(data);
+                        that.getView().setModel(DataModel, "");                         
+                        //that.getView().getModel().setData(data);
+                       // that.getView().getModel().oData = data
+                    },
+                    error: function (error) {
+                        sap.m.MessageToast.show("Error");
+                    }
+                });
+
             },
 
             _onObjectMatched: function (oEvent) {
@@ -28,26 +97,47 @@ sap.ui.define([
                 DataModelTestata.setData(dataTestata);
                 this.getView().setModel(DataModelTestata, "oModelTestata");
 
-                var that = this;
+                //oView = this.getView().byId("LineItemsSmartTable");
+              
+                //this.getView().byId("LineItemsSmartTable").setModel(oModel);
 
-                jQuery.ajax({
-                    url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/TimingDifferencesView(imposta='" + imposta + "',computationId=" + computationID + ")/Set"),
-                    contentType: "application/json",
-                    type: 'GET',
-                    dataType: "json",
-                    async: false,
-                    success: function (oCompleteEntry) {
-                        var data = {
-                            oModel: oCompleteEntry.value
-                        };
-                        var DataModel = new sap.ui.model.json.JSONModel();
-                        DataModel.setData(data);
-                        that.getView().setModel(DataModel, "oModelTiming");
-                    },
-                    error: function (error) {
-                        sap.m.MessageToast.show("Error");
-                    }
-                });
+                // var that = this;
+
+                // jQuery.ajax({
+                //     url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/DTView(imposta='IRES',computationId="+computationID+")/Set"),
+                //     contentType: "application/json",
+                //     type: 'GET',
+                //     dataType: "json",
+                //     async: false,
+                //     success: function (oCompleteEntry) {
+                //         var data = oCompleteEntry.value;
+                //         var DataModel = new sap.ui.model.json.JSONModel();
+                //         DataModel.setData(data);
+                //         that.getView().byId("LineItemsSmartTable").setModel(DataModel);
+                //     },
+                //     error: function (error) {
+                //         sap.m.MessageToast.show("Error");
+                //     }
+                // });
+
+                // jQuery.ajax({
+                //     url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/TimingDifferencesView(imposta='" + imposta + "',computationId=" + computationID + ")/Set"),
+                //     contentType: "application/json",
+                //     type: 'GET',
+                //     dataType: "json",
+                //     async: false,
+                //     success: function (oCompleteEntry) {
+                //         var data = {
+                //             oModel: oCompleteEntry.value
+                //         };
+                //         var DataModel = new sap.ui.model.json.JSONModel();
+                //         DataModel.setData(data);
+                //         that.getView().setModel(DataModel, "oModelTiming");
+                //     },
+                //     error: function (error) {
+                //         sap.m.MessageToast.show("Error");
+                //     }
+                // });
             }
         });
     });
