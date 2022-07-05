4ee81ff98abe30885fac6a0ef8bf98178206dc12
Alessio Fiordellisi <afiordellisi@deloitte.it>
2022-07-05 10:54:32 +0000

    aggiunta highlight timeDiff

--- webapp/controller/TimeDiff.controller.js
+++ webapp/controller/TimeDiff.controller.js
@@ -26,11 +26,10 @@ sap.ui.define(
         },
 
         _onObjectMatched: function (oEvent) {
-          if(oEvent.getParameter === undefined){
+          if (oEvent.getParameter === undefined) {
             var computationID = oEvent.ID;
             var imposta = oEvent.imposta;
-          }
-          else{
+          } else {
             var oEvent = oEvent.getParameter("arguments");
             var computationID = oEvent.ID;
             var imposta = oEvent.imposta;
@@ -57,9 +56,8 @@ sap.ui.define(
             async: false,
             success: function (oCompleteEntry) {
               var data = oCompleteEntry.value;
-              
 
-              var modello = [
+              var oTotale = [
                 {
                   computationId: null,
                   imposta: null,
@@ -79,65 +77,6 @@ sap.ui.define(
                 },
               ];
 
-              // var DataModel2 = new sap.ui.model.json.JSONModel();
-              // var totali = {totaleOB : 0, totalePRA : 0, totaleExt : 0, totaleCYA : 0, totaleCYU : 0, totaleOtA : 0, totaleCB : 0, totaleC1 : 0, totaleC2 : 0, totaleC3 : 0, totaleLT : 0};
-
-              for (var i = 0; i < oCompleteEntry.value.length; i++) {
-                if (oCompleteEntry.value[i].OpeningBalance) {
-                  //oCompleteEntry.value[i].OpeningBalance = 0;
-                  modello[0].OpeningBalance +=
-                    oCompleteEntry.value[i].OpeningBalance;
-                }
-
-                if (oCompleteEntry.value[i].PriorYearAdjustments) {
-                  modello[0].PriorYearAdjustments +=
-                    oCompleteEntry.value[i].PriorYearAdjustments;
-                }
-
-                if (oCompleteEntry.value[i].extraordinaryTransactions) {
-                  modello[0].extraordinaryTransactions +=
-                    oCompleteEntry.value[i].extraordinaryTransactions;
-                }
-
-                if (oCompleteEntry.value[i].CurrentYearAccrual) {
-                  modello[0].CurrentYearAccrual +=
-                    oCompleteEntry.value[i].CurrentYearAccrual;
-                }
-
-                if (oCompleteEntry.value[i].CurrentYearUtilization) {
-                  modello[0].CurrentYearUtilization +=
-                    oCompleteEntry.value[i].CurrentYearUtilization;
-                }
-
-                if (oCompleteEntry.value[i].otherAdjustments) {
-                  modello[0].otherAdjustments +=
-                    oCompleteEntry.value[i].otherAdjustments;
-                }
-
-                if (oCompleteEntry.value[i].closingBalance) {
-                  modello[0].closingBalance +=
-                    oCompleteEntry.value[i].closingBalance;
-                }
-
-                if (oCompleteEntry.value[i].current1) {
-                  modello[0].current1 += oCompleteEntry.value[i].current1;
-                }
-
-                if (oCompleteEntry.value[i].current2) {
-                  modello[0].current2 += oCompleteEntry.value[i].current2;
-                }
-
-                if (oCompleteEntry.value[i].current3) {
-                  modello[0].current3 += oCompleteEntry.value[i].current3;
-                }
-
-                if (oCompleteEntry.value[i].longTerm) {
-                  modello[0].longTerm += oCompleteEntry.value[i].longTerm;
-                }
-              }
-
-              data.push(modello[0]);
-
               for (var i = 0; i < data.length; i++) {
                 data[i].closingBalance =
                   data[i].OpeningBalance +
@@ -147,50 +86,52 @@ sap.ui.define(
                   data[i].CurrentYearUtilization +
                   data[i].otherAdjustments;
 
-                if(data[i].OpeningBalance === null){
-                    data[i].OpeningBalance = 0;
+                if (data[i].OpeningBalance === null) {
+                  data[i].OpeningBalance = 0;
                 }
 
-                if(data[i].PriorYearAdjustments === null){
-                    data[i].PriorYearAdjustments = 0;
+                if (data[i].PriorYearAdjustments === null) {
+                  data[i].PriorYearAdjustments = 0;
                 }
 
-                if(data[i].extraordinaryTransactions === null){
-                    data[i].extraordinaryTransactions = 0;
+                if (data[i].extraordinaryTransactions === null) {
+                  data[i].extraordinaryTransactions = 0;
                 }
 
-                if(data[i].CurrentYearAccrual === null){
-                    data[i].CurrentYearAccrual = 0;
+                if (data[i].CurrentYearAccrual === null) {
+                  data[i].CurrentYearAccrual = 0;
                 }
 
-                if(data[i].CurrentYearUtilization === null){
-                    data[i].CurrentYearUtilization = 0;
+                if (data[i].CurrentYearUtilization === null) {
+                  data[i].CurrentYearUtilization = 0;
                 }
 
-                if(data[i].otherAdjustments === null){
-                    data[i].otherAdjustments = 0;
+                if (data[i].otherAdjustments === null) {
+                  data[i].otherAdjustments = 0;
                 }
 
-                if(data[i].current1 === null){
-                    data[i].current1 = 0;
+                if (data[i].current1 === null) {
+                  data[i].current1 = 0;
                 }
 
-                if(data[i].current2 === null){
-                    data[i].current2 = 0;
+                if (data[i].current2 === null) {
+                  data[i].current2 = 0;
                 }
 
-                if(data[i].current3 === null){
-                    data[i].current3 = 0;
+                if (data[i].current3 === null) {
+                  data[i].current3 = 0;
                 }
 
-                if(data[i].longTerm === null){
-                    data[i].longTerm = 0;
+                if (data[i].longTerm === null) {
+                  data[i].longTerm = 0;
                 }
-                
               }
+
+              data.push(oTotale[0]);
 
               var oModel = new JSONModel(data);
               that.getView().setModel(oModel, "oModelTiming");
+              that._setTotali();
             },
             error: function (error) {
               sap.m.MessageToast.show("Error");
@@ -198,52 +139,174 @@ sap.ui.define(
           });
         },
 
-        onValueChanged: function (oEvent){
-            var path = oEvent.getSource().getParent().getBindingContext("oModelTiming").getPath().substring(1);
-            this.getView().getModel("oModelTiming").getData()[path].updated = true;
-        }, 
+        _setTotali: function () {
+          var oModel = this.getView().getModel("oModelTiming");
+          var data = oModel.getData();
+          var oTotale = this.getView().getModel("oModelTiming").getData()[
+            data.length - 1
+          ];
 
-        onPressSave: function(oEvent){
-            var modello = this.getView().getModel("oModelTiming").getData();
-            var array = [];
-                array = modello.filter(item => item.updated === true);
-            var computation = modello[0].computationId;
-            var imposta = modello[0].imposta;
-            var that = this;
+          oTotale = {
+            computationId: null,
+            imposta: null,
+            codiceRipresa: "",
+            descrizioneRipresa: "Totale",
+            OpeningBalance: 0,
+            PriorYearAdjustments: 0,
+            CurrentYearAccrual: 0,
+            CurrentYearUtilization: 0,
+            extraordinaryTransactions: 0,
+            otherAdjustments: 0,
+            closingBalance: 0,
+            current1: 0,
+            current2: 0,
+            current3: 0,
+            longTerm: 0,
+          };
 
-            var oObject = {ID: computation, imposta: imposta}
-            for(var i = 0; i < array.length; i++){
+          for (var i = 0; i < data.length - 1; i++) {
+            if (data[i].OpeningBalance) {
+              oTotale.OpeningBalance += data[i].OpeningBalance;
+            }
+
+            if (data[i].PriorYearAdjustments) {
+              oTotale.PriorYearAdjustments += data[i].PriorYearAdjustments;
+            }
+
+            if (data[i].extraordinaryTransactions) {
+              oTotale.extraordinaryTransactions +=
+                data[i].extraordinaryTransactions;
+            }
+
+            if (data[i].CurrentYearAccrual) {
+              oTotale.CurrentYearAccrual += data[i].CurrentYearAccrual;
+            }
+
+            if (data[i].CurrentYearUtilization) {
+              oTotale.CurrentYearUtilization += data[i].CurrentYearUtilization;
+            }
+
+            if (data[i].otherAdjustments) {
+              oTotale.otherAdjustments += data[i].otherAdjustments;
+            }
+
+            if (data[i].closingBalance) {
+              oTotale.closingBalance += data[i].closingBalance;
+            }
+
+            if (data[i].current1) {
+              oTotale.current1 += data[i].current1;
+            }
+
+            if (data[i].current2) {
+              oTotale.current2 += data[i].current2;
+            }
+
+            if (data[i].current3) {
+              oTotale.current3 += data[i].current3;
+            }
+
+            if (data[i].longTerm) {
+              oTotale.longTerm += data[i].longTerm;
+            }
+          }
+
+          //modelloTotali = data.concat(oTotale)
+
+          oModel.getData()[data.length - 1] = oTotale;
+
+          oModel.refresh();
+        },
+
+        onValueChanged: function (oEvent) {
+          var path = oEvent
+            .getSource()
+            .getParent()
+            .getBindingContext("oModelTiming")
+            .getPath()
+            .substring(1);
+          this.getView().getModel("oModelTiming").getData()[
+            path
+          ].updated = true;
+          this.getView().getModel("oModelTiming").getData()[path].state =
+            "Warning";
+        },
+
+        onPressSave: function (oEvent) {
+          var modello = this.getView().getModel("oModelTiming").getData();
+          var array = [];
+          array = modello.filter((item) => item.updated === true);
+          var computation = modello[0].computationId;
+          var imposta = modello[0].imposta;
+          var that = this;
+
+          var oObject = { ID: computation, imposta: imposta };
+
+          if(array.length > 0){
+            for (var i = 0; i < array.length; i++) {
                 var codiceRipresa = array[i].codiceRipresa;
-                var extraordinaryTransaction = parseFloat(array[i].extraordinaryTransactions);
-                var otherAdjustment =  parseFloat(array[i].otherAdjustments);
+                var extraordinaryTransaction = parseFloat(
+                array[i].extraordinaryTransactions
+                );
+                var otherAdjustment = parseFloat(array[i].otherAdjustments);
                 var current2 = parseFloat(array[i].current2);
                 var current3 = parseFloat(array[i].current3);
                 var longTerm = parseFloat(array[i].longTerm);
+
+                var indice = that
+                .getView()
+                .getModel("oModelTiming")
+                .getData()
+                .findIndex((x) => x.codiceRipresa === codiceRipresa);
                 var them = that;
 
-                var nuovaTiming = JSON.stringify({"extraordinaryTransactions": extraordinaryTransaction, "otherAdjustments": otherAdjustment, "current2": current2, "current3": current3, "longTerm": longTerm});
-                
-                jQuery.ajax({
-                    url: jQuery.sap.getModulePath(sap.ui.getCore().sapAppID + "/catalog/TimingDifferences(codiceRipresa='" + codiceRipresa + "',computation=" + computation + ",imposta='" + imposta + "')"),
-                    contentType: "application/json",
-                    type: 'PATCH',
-                    dataType: "json",
-                    data: nuovaTiming,
-                    async: false,
-                    success: function (oCompleteEntry) {
-                        sap.m.MessageToast.show("Success");
-                        them._onObjectMatched(oObject);
-                    },
-                    error: function (error) {
-                        sap.m.MessageToast.show("Error");
-                    }
+                var nuovaTiming = JSON.stringify({
+                extraordinaryTransactions: extraordinaryTransaction,
+                otherAdjustments: otherAdjustment,
+                current2: current2,
+                current3: current3,
+                longTerm: longTerm,
                 });
 
-                
+                jQuery.ajax({
+                url: jQuery.sap.getModulePath(
+                    sap.ui.getCore().sapAppID +
+                    "/catalog/TimingDifferences(codiceRipresa='" +
+                    codiceRipresa +
+                    "',computation=" +
+                    computation +
+                    ",imposta='" +
+                    imposta +
+                    "')"
+                ),
+                contentType: "application/json",
+                type: "PATCH",
+                dataType: "json",
+                data: nuovaTiming,
+                async: false,
+                success: function (oCompleteEntry) {
+                    them.getView().getModel("oModelTiming").getData()[
+                    indice
+                    ].state = "Success";
+                    them.getView().getModel("oModelTiming").getData()[
+                        indice
+                      ].updated = false;
+                },
+                error: function (error) {
+                    them.getView().getModel("oModelTiming").getData()[
+                    indice
+                    ].state = "Error";
+                },
+                });
             }
 
-            
-        }
+          this._setTotali();
+        
+          }
+          else{
+              sap.m.MessageToast.show(this.getResourceBundle().getText("valorizzareRecord"));
+          }
+        },
       }
     );
   }
